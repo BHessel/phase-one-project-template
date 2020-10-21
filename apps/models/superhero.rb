d46e459e7136d9a517ad1293a104292a3318bbb5
BHessel <hesselfitness@gmail.com>
2020-10-21 10:51:38 -0400

    improved relationships

--- apps/models/superhero.rb
+++ apps/models/superhero.rb
@@ -1,4 +1,7 @@
 class Superhero < ActiveRecord::Base
-    has_many :powers
-    has_many :comics
+    has_many :superhero_powers
+    has_many :powers, through: :superhero_powers
+    has_many :superhero_comics
+    has_many :comics, through: :superhero_comics
+    #is the order of this right?
 end
