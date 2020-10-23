4e759f5dff243258ae5d915c579aee7064b706f0
BHessel <hesselfitness@gmail.com>
2020-10-23 14:22:00 -0400

    scooter complete

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -1,6 +1,8 @@
 
 class CLI 
     
+    $prompt = TTY::Prompt.new
+
     def run 
         welcome
         main_menu
@@ -19,8 +21,7 @@ class CLI
 
     
     def main_menu
-        prompt = TTY::Prompt.new
-        menuchoice = prompt.select("What would you like to look up first?") do |menu|
+        menuchoice = $prompt.select("What would you like to look up first?") do |menu|
            
             menu.choice "Show me a Superheroes powers", 1
             menu.choice "Show all comics a Superhero is in", 2
@@ -56,8 +57,6 @@ class CLI
     end
 
     def character_list_helper
-        prompt = TTY::Prompt.new
-        
         shero = Superhero.pluck(:id, :name)
         super_hash = {}
         
@@ -66,68 +65,51 @@ class CLI
         super_hash[hero[1]] = hero[0]
         end
 
-        #binding.pry
-        prompt.select("Choose your hero:", super_hash)#, per_page: 10)
+        $prompt.select("Choose your hero:", super_hash, per_page: 10)
     end
 
 
     def author_list_helper
-        prompt = TTY::Prompt.new
-        prompt.select("Select an author", per_page: 10) do |menu|
-
-            menu.choice Comic.find_by(id: 1)["author"], 1
-            menu.choice Comic.find_by(id: 2)["author"], 2
-            menu.choice Comic.find_by(id: 3)["author"], 3
-            menu.choice Comic.find_by(id: 4)["author"], 4
-            menu.choice Comic.find_by(id: 5)["author"], 5
-            menu.choice Comic.find_by(id: 6)["author"], 6
-            menu.choice Comic.find_by(id: 7)["author"], 7
-            menu.choice "Return to Main Menu", 8
-        end
+        author = Comic.group(:author).pluck(:author)
     end
 
-    def comic_list_helper
-        #allows user to select from all comics
-        prompt = TTY::Prompt.new
-        prompt.select("Select a comic", per_page: 10) do |menu|
 
-            menu.choice Comic.find_by(id: 1)["title"], 1
-            menu.choice Comic.find_by(id: 2)["title"], 2
-            menu.choice Comic.find_by(id: 3)["title"], 3
-            menu.choice Comic.find_by(id: 4)["title"], 4
-            menu.choice Comic.find_by(id: 5)["title"], 5
-            menu.choice Comic.find_by(id: 6)["title"], 6
-            menu.choice Comic.find_by(id: 7)["title"], 7
-            menu.choice "Return to Main Menu", 8
+    def comic_list_helper    #allows user to select from all comics
+        comic_list = Comic.pluck(:id, :title)
+        comic_hash = {}
+
+        comic_list.each do |comic|
+            comic_hash[comic[1]] = comic[0]
         end
+
+        $prompt.select("Select an comic:", comic_hash, per_page: 10) 
     end
 
 
     def select_superhero_show_powers
-        prompt = TTY::Prompt.new
         herochoice = character_list_helper
 
         if herochoice == 1
             #shows Wolverine's powers, returns to main menu after 5 sec
-            Superhero.find_by(id: 1).show_powers 
+            puts Superhero.find_by(id: 1).show_powers 
             return_to_main
         elsif herochoice == 2
-            Superhero.find_by(id: 2).show_powers
+            puts Superhero.find_by(id: 2).show_powers
             return_to_main
         elsif herochoice == 3
-            Superhero.find_by(id: 3).show_powers
+            puts Superhero.find_by(id: 3).show_powers
             return_to_main
         elsif herochoice == 4
-            Superhero.find_by(id: 4).show_powers
+            puts Superhero.find_by(id: 4).show_powers
             return_to_main
         elsif herochoice == 5
-            Superhero.find_by(id: 5).show_powers
+            puts Superhero.find_by(id: 5).show_powers
             return_to_main
         elsif herochoice == 6
-            Superhero.find_by(id: 6).show_powers
+            puts Superhero.find_by(id: 6).show_powers
             return_to_main
         elsif herochoice == 7
-            Superhero.find_by(id: 7).show_powers
+            puts Superhero.find_by(id: 7).show_powers
             return_to_main
         else herochoice == 8
             main_menu
@@ -137,29 +119,28 @@ class CLI
 
 
     def select_hero_show_comics
-        prompt = TTY::Prompt.new
         herocomics = character_list_helper
 
         if herocomics == 1
-            Superhero.find_by(id: 1).superhero_featured_in
+            puts Superhero.find_by(id: 1).superhero_featured_in
             return_to_main
         elsif herocomics == 2
-            Superhero.find_by(id: 2).superhero_featured_in
+            puts Superhero.find_by(id: 2).superhero_featured_in
             return_to_main
         elsif herocomics == 3
-            Superhero.find_by(id: 3).superhero_featured_in
+            puts Superhero.find_by(id: 3).superhero_featured_in
             return_to_main
         elsif herocomics == 4
-            Superhero.find_by(id: 4).superhero_featured_in
+            puts Superhero.find_by(id: 4).superhero_featured_in
             return_to_main
         elsif herocomics == 5
-            Superhero.find_by(id: 5).superhero_featured_in
+            puts Superhero.find_by(id: 5).superhero_featured_in
             return_to_main
         elsif herocomics == 6
-            Superhero.find_by(id: 6).superhero_featured_in
+            puts Superhero.find_by(id: 6).superhero_featured_in
             return_to_main
         elsif herocomics == 7
-            Superhero.find_by(id: 7).superhero_featured_in
+            puts Superhero.find_by(id: 7).superhero_featured_in
             return_to_main
         else herocomics == 8
             main_menu
@@ -168,60 +149,37 @@ class CLI
 
 
     def author_shows_comics
-        prompt = TTY::Prompt.new
-        author = author_list_helper
-
-        if author == 1
-            Comic.all_comics_by_author(1)
-            return_to_main
-        elsif author == 2
-            Comic.all_comics_by_author(2)
-            return_to_main
-        elsif author == 3
-            Comic.all_comics_by_author(3)
-            return_to_main
-        elsif author == 4
-            Comic.all_comics_by_author(4)
-            return_to_main
-        elsif author == 5
-            Comic.all_comics_by_author(5)
-            return_to_main
-        elsif author == 6
-            Comic.all_comics_by_author(6)
-            return_to_main
-        elsif author == 7
-            Comic.all_comics_by_author(7)  
-            return_to_main
-        else author == 8
-            main_menu
-        end
+        @selected_auth = $prompt.select("Select an author:", author_list_helper, per_page: 7) 
+        all_commics = Comic.all_comics_by_author(@selected_auth)
+        puts all_commics
+        return_to_main
     end
 
 
+
     def superhero_comic_count
-        prompt = TTY::Prompt.new
         herocount = character_list_helper
 
         if herocount == 1
-            Superhero.find_by(id: 1).comic_count
+            puts Superhero.find_by(id: 1).comic_count
             return_to_main
         elsif herocount == 2
-            Superhero.find_by(id: 2).comic_count
+            puts Superhero.find_by(id: 2).comic_count
             return_to_main
         elsif herocount == 3
-            Superhero.find_by(id: 3).comic_count
+            puts Superhero.find_by(id: 3).comic_count
             return_to_main
         elsif herocount == 4
-            Superhero.find_by(id: 4).comic_count
+            puts Superhero.find_by(id: 4).comic_count
             return_to_main
         elsif herocount == 5
-            Superhero.find_by(id: 5).comic_count
+            puts Superhero.find_by(id: 5).comic_count
             return_to_main
         elsif herocount == 6
-            Superhero.find_by(id: 6).comic_count
+            puts Superhero.find_by(id: 6).comic_count
             return_to_main
         elsif herocount == 7
-            Superhero.find_by(id: 7).comic_count
+            puts Superhero.find_by(id: 7).comic_count
             return_to_main
         else herocount == 8
             main_menu
@@ -231,29 +189,28 @@ class CLI
 
 
     def comic_show_author
-        prompt = TTY::Prompt.new
         comicauthor = comic_list_helper
 
         if comicauthor == 1
-            Comic.find_by(id: 2).find_author
+            puts Comic.find_by(id: 1).find_author
             return_to_main
         elsif comicauthor == 2
-            Comic.find_by(id: 2).find_author
+            puts Comic.find_by(id: 2).find_author
             return_to_main
         elsif comicauthor == 3
-            Comic.find_by(id: 3).find_author
+            puts Comic.find_by(id: 3).find_author
             return_to_main
         elsif comicauthor == 4
-            Comic.find_by(id: 4).find_author
+            puts Comic.find_by(id: 4).find_author
             return_to_main
         elsif comicauthor == 5
-            Comic.find_by(id: 5).find_author
+            puts Comic.find_by(id: 5).find_author
             return_to_main
         elsif comicauthor == 6
-            Comic.find_by(id: 6).find_author
+            puts Comic.find_by(id: 6).find_author
             return_to_main
         elsif comicauthor == 7
-            Comic.find_by(id: 7).find_author
+            puts Comic.find_by(id: 7).find_author
             return_to_main
         else comicauthor == 8
             main_menu
